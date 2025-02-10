783018f1fc75ef4465dd8ef26711b5f87d1336a3
Sergeant Svyatskiy <sithserega@gmail.com>
2025-02-10 20:43:11 +0300

    fix HeaderPage

--- src/main/java/pages/HeaderPage.java
+++ src/main/java/pages/HeaderPage.java
@@ -9,9 +9,14 @@ import java.util.List;
 public class HeaderPage extends BasePage{
 
     public static final By CART_BUTTON = By.cssSelector(".shopping_cart_link");
+    public static final By PRODUCT_QUANTITY_ON_THE_CART_ICON = By.cssSelector(".shopping_cart_badge");
 
     public HeaderPage(WebDriver driver) {
         super(driver);
+    }
+
+    public String getTextFromIcon(){
+        return driver.findElement(PRODUCT_QUANTITY_ON_THE_CART_ICON).getText();
     }
 
     public void clickCartButton(){
