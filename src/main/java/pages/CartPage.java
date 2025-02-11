cb9ce6bffd51a4f3bd2ce88e159529c9f91a5216
Sergeant Svyatskiy <sithserega@gmail.com>
2025-02-11 18:45:13 +0300

    fix CartPage (add new logic)

--- src/main/java/pages/CartPage.java
+++ src/main/java/pages/CartPage.java
@@ -10,16 +10,24 @@ import java.util.List;
 public class CartPage extends HeaderPage{
     public static final By CHECKOUT_BUTTON = By.id("checkout");
     public static final By CONTINUE_SHOPPING_BUTTON = By.id("continue-shopping");
-    public static final By REMOVE_BUTTON = By.xpath("//*[contains(text(),'Remove')]");
-    public static final By CART_ITEM = By.cssSelector(".cart_item");
     public static final By NAMES_PRODUCTS_IN_CART = By.cssSelector(".inventory_item_name");
+    public static final By CART_PRODUCT = By.cssSelector(".cart_item");
+    public static final String CART_ITEM = "//*[text()='%s']/ancestor::*[@class='cart_item']";
+    public static final String REMOVE_PRODUCT_FROM_CART_BUTTON = CART_ITEM + "//button[text()='Remove']";
+    public static final String PRODUCT_PRICE = CART_ITEM + "//*[@data-test='inventory-item-price']";
 
     public CartPage(WebDriver driver) {
         super(driver);
     }
 
     public List<WebElement> getAllProductsInShoppingCart(){
-        return driver.findElements(CART_ITEM);
+        return driver.findElements(CART_PRODUCT);
+    }
+
+    public void removeProductFromCart(String... productName){
+        for (String productsNames : productName){
+            driver.findElement(By.xpath(String.format(REMOVE_PRODUCT_FROM_CART_BUTTON, productsNames))).click();
+        }
     }
 
     public List<String> getNamesProductsInShoppingCart(){
@@ -30,16 +38,14 @@ public class CartPage extends HeaderPage{
         return list;
     }
 
-    public String getPrice(){
-//        return driver.findElement(By.xpath("//*[text()='7.99']")).getText();
+    public double getPrice(String productName){
+        return Double.parseDouble(driver.findElement(By.xpath(String.format(PRODUCT_PRICE, productName)))
+                .getText()
+                .replace("$",""));
     }
 
     public int getQuantityOfAllGoods(){
         return getAllProductsInShoppingCart().size();
-    }
-
-    public void removeButton(){
-        driver.findElement(REMOVE_BUTTON).click();
     }
 
     public void checkoutButton(){
