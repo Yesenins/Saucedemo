45ceb4af6b2bc070a1d624b174479ec3616a5804
Sergeant Svyatskiy <sithserega@gmail.com>
2025-02-11 18:46:02 +0300

    CheckoutPage add new method

--- src/main/java/pages/CheckoutPage.java
+++ src/main/java/pages/CheckoutPage.java
@@ -8,11 +8,17 @@ public class CheckoutPage extends CartPage{
     public static final By LASTNAME_INPUT = By.id("last-name");
     public static final By ZIP_CODE = By.id("postal-code");
     public static final By CONTINUE_BUTTON = By.id("continue");
+    public static final By ITEM_TOTAL = By.cssSelector(".summary_subtotal_label");
+
 
     public CheckoutPage(WebDriver driver) {
         super(driver);
     }
 
+    public double getItemTotal(){
+        return Double.parseDouble(driver.findElement(ITEM_TOTAL).getText()
+                .replace("Item total: $",""));
+    }
     public void postalInformationCompletion(String firstName, String lastName){
         driver.findElement(FIRSTNAME_INPUT).sendKeys(firstName);
         driver.findElement(LASTNAME_INPUT).sendKeys(lastName);
