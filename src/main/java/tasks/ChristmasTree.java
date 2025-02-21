9d5d9718eddea625d05e58608db2f096cc809bb2
Sergeant Svyatskiy <sithserega@gmail.com>
2025-02-21 17:40:58 +0300

    add task 6

---
+++ src/main/java/tasks/ChristmasTree.java
@@ -0,0 +1,27 @@
+package tasks;
+
+import java.util.Scanner;
+
+public class ChristmasTree {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter you're number: " );
+        int number = scanner.nextInt();
+        for (int i = 1; i <= number; i++){
+            for (int k = number-i; k >= 0; k--){
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++){
+                System.out.print("$");
+            }
+            for (int m = 1; m < i; m++){
+                System.out.print("$");
+            }
+            for (int l = number-i; l >= 0; l--){
+                System.out.print(" ");
+            }
+            System.out.println("");
+
+        }
+    }
+}
