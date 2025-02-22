9ff2d96862686a565c6e0ef3fd2865b5c1898f44
Sergeant Svyatskiy <sithserega@gmail.com>
2025-02-22 17:38:34 +0300

    add Waiters.java

---
+++ src/main/java/waiters/Waiters.java
@@ -0,0 +1,19 @@
+package waiters;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class Waiters {
+
+    public void waitForPageLoaded() {
+        new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver driver) {
+                return ((JavascriptExecutor) driver).executeScript("return document.readyState").toString().equals("complete");
+            }
+        };
+    }
+}
