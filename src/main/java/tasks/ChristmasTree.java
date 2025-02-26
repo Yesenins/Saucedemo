e925f0621c77f78711828edc66a86821b2bf94c3
Sergeant Svyatskiy <sithserega@gmail.com>
2025-02-26 18:48:34 +0300

    fix ChristmasTree

--- src/main/java/tasks/ChristmasTree.java
+++ src/main/java/tasks/ChristmasTree.java
@@ -21,7 +21,18 @@ public class ChristmasTree {
                 System.out.print(" ");
             }
             System.out.println("");
+        }
 
+        for (int i = 1; i <= number; i++) {
+
+            for (int j = number - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
